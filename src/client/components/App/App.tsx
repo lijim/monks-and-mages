2aa097e039f689fecfd51a2530ae4de77a1bb7d6
Jimmy Li <li7.jim@gmail.com>
2022-02-25 01:25:53 -0500

    Implement routing
    After some agonizing, I discovered that the latest version of react-router-dom (v6) is supported by redux-first-history
    
    Connected-react-router seems to have not been maintained to support react-router v6: https://github.com/supasate/connected-react-router/issues/543

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { useDispatch } from 'react-redux';
+import { HistoryRouter as Router } from 'redux-first-history/rr6';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import { DeckList } from '../DeckList';
 import { CompactDeckList } from '../CompactDeckList';
 import { makeSampleDeck1 } from '@/factories/deck';
 import { disconnectUser, initializeUser } from '@/client/redux/user';
+import { AppDispatch, history } from '@/client/redux/store';
 
 export const App: React.FC = () => {
-    const dispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
     const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
     useEffect(() => {
         socket.on('connect', () => {
@@ -23,8 +26,22 @@ export const App: React.FC = () => {
 
     return (
         <div>
-            <DeckList deck={deck} />
-            <CompactDeckList deck={deck} />
+            <Router history={history}>
+                <React.Fragment>
+                    <Link to="/">Deck List 1</Link>
+                    <br />
+                    <Link to="/compact">Compact Deck List</Link>
+
+                    <Routes>
+                        <Route path="/" element={<DeckList deck={deck} />} />
+                        <Route
+                            path="/compact"
+                            element={<CompactDeckList deck={deck} />}
+                        />
+                        <Route element={<DeckList deck={deck} />} />
+                    </Routes>
+                </React.Fragment>
+            </Router>
         </div>
     );
 };
--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -1,15 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { createReduxHistoryContext } from 'redux-first-history';
+import { createBrowserHistory } from 'history';
+import { applyMiddleware } from 'redux';
+
 import { userReducer, userSlice } from './user';
 
-export const store = configureStore({
-    reducer: {
+const { createReduxHistory, routerMiddleware, routerReducer } =
+    createReduxHistoryContext({
+        history: createBrowserHistory(),
+    });
+
+const createRootReducer = () =>
+    combineReducers({
+        router: routerReducer,
         user: userReducer,
-    },
+    });
+
+export const store = configureStore({
+    reducer: createRootReducer(),
     devTools: true,
+    enhancers: [applyMiddleware(routerMiddleware)],
     preloadedState: {
         user: userSlice.getInitialState(),
     },
 });
+export const history = createReduxHistory(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
