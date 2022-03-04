64cf017451ed8ee00c524bb04d63407ae39d8fe9
Jimmy Li <li7.jim@gmail.com>
2022-03-03 22:37:37 -0500

    Display current vs. other players
    - This PR moves the logic for redirecting over into the express side, instead of the app side.
    
    - Added selectors for displaying the current player + the other players (#38)
    
    - light reconfiguration of GameBoard to demo showing off the current vs. other player paradigm
    
    Next up:
    #39

--- src/client/components/GameBoard/GameBoard.tsx
+++ src/client/components/GameBoard/GameBoard.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { getCurrentPlayer, getOtherPlayers } from '@/client/redux/selectors';
 import { RootState } from '@/client/redux/store';
 import { Player } from '@/types/board';
+import { CardGridItem } from '../CardGridItem';
 
 export const GameBoard: React.FC = () => {
-    const players = useSelector<RootState, Player[]>(
-        (state) => state.board?.players || []
+    const currentPlayer = useSelector<RootState, Player | null>(
+        getCurrentPlayer
     );
+    const otherPlayers = useSelector<RootState, Player[]>(getOtherPlayers);
 
     return (
         <div>
             Game started
-            {players.map((player) => (
-                <li key={player.name}>{player.name}</li>
+            {currentPlayer && (
+                <>
+                    <li>
+                        <b>{currentPlayer.name}</b>
+                    </li>
+                    {currentPlayer.hand.map((card) => (
+                        <CardGridItem card={card} />
+                    ))}
+                </>
+            )}
+            {otherPlayers.map((player) => (
+                <li key={player.name}>
+                    <b>{player.name}</b>
+                    <br />
+                    Cards in Hand: {player.numCardsInHand}
+                </li>
             ))}
         </div>
     );
--- src/client/redux/selectors.ts
+++ src/client/redux/selectors.ts
@@ -1,4 +1,29 @@
+import { Player } from '@/types/board';
 import { RootState } from './store';
 
 export const isUserInitialized = (state: Partial<RootState>): boolean =>
     !!state.user.name;
+
+// get the Player
+export const getCurrentPlayer = (state: Partial<RootState>): Player | null => {
+    if (!state.board?.players) return null;
+
+    return (state.board.players || []).find(
+        (player) => player.name === state.user.name
+    );
+};
+
+// get other players, by turn order
+export const getOtherPlayers = (state: Partial<RootState>): Player[] => {
+    if (!state.board?.players) return [];
+
+    const indexOfCurrentPlayer = state.board.players.findIndex(
+        (player) => player?.name === state.user.name
+    );
+    if (indexOfCurrentPlayer === -1) {
+        return state.board.players;
+    }
+    const nextPlayers = state.board.players.slice(indexOfCurrentPlayer + 1);
+    const prevPlayers = state.board.players.slice(0, indexOfCurrentPlayer);
+    return [...nextPlayers, ...prevPlayers];
+};
