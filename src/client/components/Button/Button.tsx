6c54e09888ca0b54f8e0096ec76d59dc704f9f13
Jimmy Li <li7.jim@gmail.com>
2022-03-07 00:14:07 -0500

    Stylize intro screen

---
+++ src/client/components/Button/Button.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Colors } from '@/constants/colors';
+
+interface ButtonProps {
+    backgroundColor: string;
+    borderColor?: string;
+    emoji?: string;
+    hoverBackgroundColor: string;
+}
+
+export const Button = styled.button.attrs(
+    ({
+        backgroundColor,
+        borderColor = Colors.DARK_BROWN,
+        emoji = '',
+        hoverBackgroundColor,
+    }: ButtonProps) => ({
+        backgroundColor,
+        borderColor,
+        emoji,
+        hoverBackgroundColor,
+    })
+)`
+    ::before {
+        content: '${({ emoji }) => emoji}';
+        position: absolute;
+        left: 8px;
+    }
+    :not(:disabled):hover {
+        background: ${({ hoverBackgroundColor }) => hoverBackgroundColor};
+    }
+    :not(:disabled) {
+        box-shadow: 0 1px 2px rgb(0 0 0 / 50%);
+    }
+    :active {
+        top: 2px;
+        left: 2px;
+        box-shadow: none;
+    }
+    :disabled {
+        opacity: 0.4;
+    }
+    position: relative;
+    cursor: pointer;
+    width: 150px;
+    color: white;
+    font-size: 22px;
+    border: 1px solid ${({ borderColor }) => borderColor};
+    border-radius: 4px;
+    background: ${({ backgroundColor }) => backgroundColor};
+    height: 40px;
+`;
+
+export const PrimaryColorButton = styled(Button).attrs({
+    backgroundColor: Colors.FIRE_ORANGE,
+    emoji: '▶️',
+    hoverBackgroundColor: Colors.FIRE_ORANGE_EMPHASIZED,
+})``;
--- src/client/components/NameChanger/NameChanger.tsx
+++ src/client/components/NameChanger/NameChanger.tsx
@@ -1,8 +1,32 @@
 import React, { FormEvent, useState } from 'react';
+import styled from 'styled-components';
+import { PrimaryColorButton } from '../Button';
 
 interface NameChangerProps {
     handleSubmit: (name: string) => void;
 }
+
+const NameChangerContainer = styled.div`
+    width: 700px;
+    margin: auto;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+`;
+
+const NameChangerForm = styled.form`
+    display: grid;
+    grid-gap: 12px;
+
+    label {
+        display: block;
+        margin-bottom: 4px;
+    }
+    input {
+        height: 40px;
+        width: 300px;
+    }
+`;
 
 /**
  * The name changer component is a presentational component that takes
@@ -18,22 +42,31 @@ export const NameChanger: React.FC<NameChangerProps> = ({ handleSubmit }) => {
         handleSubmit(name);
     };
     return (
-        <>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="name-selector">Select a Name</label>
-                <input
-                    id="name-selector"
-                    role="textbox"
-                    autoComplete="off" // ignore for password managers
-                    data-lpignore="true" // ignore for lastPass
-                    data-form-type="other" // ignore for dashlane
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <button role="button" type="submit">
-                    Submit
-                </button>
-            </form>
-        </>
+        <NameChangerContainer>
+            <NameChangerForm onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="name-selector">Choose a Name</label>
+                    <input
+                        id="name-selector"
+                        role="textbox"
+                        autoFocus
+                        autoComplete="off" // ignore for password managers
+                        data-lpignore="true" // ignore for lastPass
+                        data-form-type="other" // ignore for dashlane
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <PrimaryColorButton
+                        role="button"
+                        type="submit"
+                        disabled={!name}
+                    >
+                        Start
+                    </PrimaryColorButton>
+                </div>
+            </NameChangerForm>
+        </NameChangerContainer>
     );
 };
