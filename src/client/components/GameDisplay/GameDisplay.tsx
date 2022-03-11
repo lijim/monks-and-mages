a95f2d586c51cfbb25b834d5b9592b945c796121
Jimmy Li <li7.jim@gmail.com>
2022-03-10 21:59:40 -0500

    Add visual indicator of current effect
    Closes: #96

--- src/client/components/GameDisplay/GameDisplay.tsx
+++ src/client/components/GameDisplay/GameDisplay.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-import { getOtherPlayers, getSelfPlayer } from '@/client/redux/selectors';
+import {
+    getLastEffect,
+    getOtherPlayers,
+    getSelfPlayer,
+} from '@/client/redux/selectors';
 import { RootState } from '@/client/redux/store';
-import { Colors } from '@/constants/colors';
 import { Player } from '@/types/board';
 import { SelfPlayerInfo } from '../SelfPlayerInfo';
 import { OtherPlayerInfo } from '../OtherPlayerInfo';
 import { HandOfCards } from '../HandOfCards';
 import { PlayerBoardSection } from '../PlayerBoardSection';
+import { Effect } from '@/types/cards';
+import { transformEffectToRulesText } from '@/transformers/transformEffectsToRulesText';
 
 const GameGrid = styled.div`
     width: 100%;
@@ -33,12 +38,21 @@ const CenterColumn = styled.div`
 `;
 
 const RightColumn = styled.div`
-    border: 1px solid ${Colors.DARK_BROWN};
+    margin: 4px;
+`;
+
+const EmphText = styled.span`
+    display: block;
+    text-align: center;
+    padding: 10px;
+    background: brown;
+    color: white;
 `;
 
 export const GameDisplay: React.FC = () => {
     const selfPlayer = useSelector<RootState, Player>(getSelfPlayer);
     const otherPlayers = useSelector<RootState, Player[]>(getOtherPlayers);
+    const lastEffect = useSelector<RootState, Effect>(getLastEffect);
 
     return (
         <GameGrid>
@@ -60,7 +74,13 @@ export const GameDisplay: React.FC = () => {
                 <PlayerBoardSection player={selfPlayer} isSelfPlayer />
                 <HandOfCards />
             </CenterColumn>
-            <RightColumn>Right Chat Column</RightColumn>
+            <RightColumn>
+                {lastEffect && (
+                    <EmphText>
+                        Resolving: {transformEffectToRulesText(lastEffect)}
+                    </EmphText>
+                )}
+            </RightColumn>
         </GameGrid>
     );
 };
