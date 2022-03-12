a89f38e525533028363b39db92591392c666d3ef
Jimmy Li <li7.jim@gmail.com>
2022-03-11 19:04:34 -0500

    Force https redirects on prod

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,6 +7,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+// Done to force https
+// see: https://docs.divio.com/en/latest/how-to/node-express-force-https/
+app.enable('trust proxy');
+
+// eslint-disable-next-line consistent-return
+app.use((request, response, next): void | undefined => {
+    if (process.env.NODE_ENV !== 'development' && !request.secure) {
+        return response.redirect(
+            `https://${request.headers.host}${request.url}`
+        );
+    }
+
+    next();
+});
+
 // Serves everything from dist/client as /client, e.g. http://localhost:3000/client/index.js
 app.use('/client.bundle.js', (_, res) => {
     res.sendFile(path.join(__dirname, 'client.bundle.js'));
