2a91d725ed45903b6664ba6cfe7234070c9d7ce2
Jimmy Li <li7.jim@gmail.com>
2022-03-19 23:57:19 -0400

    Add jest cache
    google 'jest cache' and found this setup from microsoft's charts parts library:
    
    https://github.com/microsoft/chart-parts/pull/71

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+    cacheDirectory: ".jest-cache",
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleDirectories: ['node_modules', 'src'],
