c462eda953c874e470febbf9177f9aa0c37a8edb
Jimmy Li <1839462+lijim@users.noreply.github.com>
2023-04-10 22:01:50 -0400

    Add cards for iron+bamboo+water combo

--- src/cardDb/units/tribes/monks.ts
+++ src/cardDb/units/tribes/monks.ts
@@ -1,6 +1,6 @@
 import { makeUnitCard as makeCard } from '@/factories/cards';
 import { CardRarity, UnitCard } from '@/types/cards';
-import { EffectType, PassiveEffect } from '@/types/effects';
+import { EffectType, PassiveEffect, TargetTypes } from '@/types/effects';
 import { Resource } from '@/types/resources';
 
 const ASSASSIN: UnitCard = makeCard({
@@ -83,8 +83,11 @@ const SHADOW_STRIKER: UnitCard = makeCard({
 });
 
 const TIGER_GENERAL: UnitCard = makeCard({
+    artistName: 'Danieloov',
+    artistUrl: 'https://pixabay.com/users/danieloov-17926627/',
+    originalImagePage:
+        'https://pixabay.com/photos/fantasy-warrior-fight-epic-heroine-6090734/',
     name: 'Tiger General',
-    // https://pixabay.com/photos/fantasy-warrior-fight-epic-heroine-6090734/
     imgSrc: 'https://cdn.pixabay.com/photo/2021/03/12/23/41/fantasy-6090734_1280.jpg',
     cost: {
         [Resource.IRON]: 1,
@@ -196,6 +199,73 @@ const MODEST_CLERIC: UnitCard = makeCard({
     rarity: CardRarity.UNCOMMON,
 });
 
+const DAWNMIST_CHARGER: UnitCard = makeCard({
+    artistName: 'Danieloov',
+    artistUrl: 'https://pixabay.com/users/danieloov-17926627/',
+    originalImagePage:
+        'https://pixabay.com/photos/horse-sunset-landscape-fantasy-fog-6041888/',
+    name: 'Dawnmist Charger',
+    imgSrc: 'https://cdn.pixabay.com/photo/2021/02/23/00/06/horse-6041888_1280.jpg',
+    cost: {
+        [Resource.IRON]: 1,
+        [Resource.BAMBOO]: 1,
+        [Resource.WATER]: 1,
+        [Resource.GENERIC]: 2,
+    },
+    description: '',
+    enterEffects: [
+        {
+            type: EffectType.BUFF_ATTACK_FOR_TURN,
+            strength: 1,
+            target: TargetTypes.ALL_SELF_UNITS,
+        },
+    ],
+    totalHp: 4,
+    attack: 3,
+    numAttacks: 1,
+    isRanged: false,
+    isMagical: false,
+    isSoldier: false,
+    passiveEffects: [PassiveEffect.QUICK],
+    rarity: CardRarity.UNCOMMON,
+});
+
+const KEEPER_OF_THE_GATE: UnitCard = makeCard({
+    artistName: 'Roman Kogomachenko',
+    artistUrl: 'https://pixabay.com/users/reflex_production-26016661/',
+    originalImagePage:
+        'https://pixabay.com/photos/torii-traditional-japanese-gate-7256271/',
+    name: 'Keeper of the Gate',
+    imgSrc: 'https://cdn.pixabay.com/photo/2022/06/11/12/35/torii-7256271_1280.jpg',
+    cost: {
+        [Resource.IRON]: 1,
+        [Resource.BAMBOO]: 1,
+        [Resource.WATER]: 1,
+        [Resource.GENERIC]: 1,
+    },
+    description: '',
+    enterEffects: [
+        {
+            type: EffectType.EXTRACT_CARD,
+            strength: 1,
+            cardName: 'Throw Shuriken',
+        },
+        {
+            type: EffectType.EXTRACT_CARD,
+            strength: 1,
+            cardName: 'Archery at Sunset',
+        },
+    ],
+    totalHp: 3,
+    attack: 2,
+    numAttacks: 1,
+    isRanged: false,
+    isMagical: false,
+    isSoldier: false,
+    passiveEffects: [PassiveEffect.QUICK],
+    rarity: CardRarity.UNCOMMON,
+});
+
 export const MONKS = {
     ASSASSIN,
     DISCIPLINED_WARRIOR,
@@ -205,4 +275,6 @@ export const MONKS = {
     NIGHTWOOD_RONIN,
     ELK_RIDER_SCOUT,
     MODEST_CLERIC,
+    DAWNMIST_CHARGER,
+    KEEPER_OF_THE_GATE,
 };
