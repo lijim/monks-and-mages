45adbe3c87aca3329d19002d406e018a66360e95
Jimmy Li <1839462+lijim@users.noreply.github.com>
2023-04-23 01:24:12 -0400

    Update effects.ts

--- src/types/effects.ts
+++ src/types/effects.ts
@@ -131,5 +131,5 @@ export enum PassiveEffect {
     HEARTY = 'Hearty (Rather than going to cemetery, lose hearty and go to 1 hp)',
     POISONED = 'Poisonous (deals lethal damage)', // deals lethal dmg to enemy units
     QUICK = 'Quick (can attack right away)', // no summoning sickness
-    STEADY = 'Steady (cannot receive buffs or nerfs to attack or HP)',
+    STEADY = "Steady (this unit's stats cannot be modified)",
 }
